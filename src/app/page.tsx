357aac50883b2cb2b89afdf031e98fd0703d14ec
mikaelbashir <bashirm-y16@mgs.org>
2025-03-09 11:50:51 +0000

    top part of home page complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,24 +18,37 @@ export default function Home() {
             alt="background image"
           />
           {/* Text Overlay */}
-          <div className="absolute left-6 top-12 z-10 text-white text-3xl">
-            <p>Empowering communities</p>
-            <p>through sports and</p>
-            <p>sustainability</p>
+          <div className="absolute flex flex-col left-[10%] top-48 z-10 text-white w-[50%] leading-none">
+            <p className="text-[36pt] font-extrabold">Empowering communities</p>
+            <p className="text-[36pt] font-extrabold">through sports and</p>
+            <p className="text-[36pt] font-extrabold">sustainability</p>
+            <p className="mt-5 font-semibold text-[15pt] italic">Sembeza Africa is committed to transforming</p>
+            <p className="mt-[2px] font-semibold text-[15pt] italic">lives in Uganda by using sports to promote health,</p>
+            <p className="mt-[2px] font-semibold text-[15pt] italic">education, community development, and</p>
+            <p className="mt-[2px] font-semibold text-[15pt] italic">environmental sustainability.</p>
           </div>
 
         </div>
         {/* Content Section */}
         <div className="relative bg-[#505A28] h-[500px] w-full text-white text-2xl text-center">
           <p className="mt-3 pt-4 px-6 font-bold">Why is our intervention needed?</p> {/* Added padding */}
-          <div className="grid grid-cols-3 gap-4 mt-14">
+          <div className="grid grid-cols-3 gap-4 mt-16">
             <div className="col-span-1 flex flex-col justify-center">
               <p className="text-4xl font-extrabold">50,403,083</p>
               <p className=" mt-2 text-[13pt] font-bold">Total population in 2024</p>
             </div>
-            <div className="col-span-1">Box 2</div>
-            <div className="col-span-1">Box 3</div>
-            <div className="col-span-1 mt-44">Box 4</div>
+            <div className="col-span-1 flex flex-col justify-center">
+              <p className="text-4xl font-extrabold">77%</p>
+              <p className=" mt-2 text-[13pt] font-bold">Of the population are below 25</p>
+            </div>
+            <div className="col-span-1 flex flex-col justify-center">
+              <p className="text-4xl font-extrabold">16.7</p>
+              <p className=" mt-2 text-[13pt] font-bold">Average population age</p>
+            </div>
+            <div className="col-span-1 flex flex-col justify-center mt-40">
+              <p className="text-4xl font-extrabold">35%</p>
+              <p className=" mt-2 text-[13pt] font-bold">Of women give birth by 18</p>
+            </div>
           </div>
         </div>
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,11 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontWeight: {
+        'xlight': 100,  // Custom font weight, 100 (extra light)
+        'medium': 500,  // Custom font weight, 500 (medium)
+        'heavy': 800,   // Custom font weight, 800 (heavy)
+      },
     },
   },
   plugins: [],
