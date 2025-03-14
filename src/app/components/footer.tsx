6bd1d48d4c5d300144a947cc06a46a869ae5e432
iamwairimu <kmnleah@gmail.com>
2025-03-14 18:53:24 +0200

    Optimised the whatwedo pages

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,86 +1,87 @@
 "use client";
 
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function Footer() {
-
   return (
     <footer className="border-t-6 border-[#F6F4DE] w-full bg-[#F6F4DE]">
-        <div className="relative bg-[#505A28] w-[100%] md:w-[40%] text-[#F6F4DE] text-2xl flex flex-col pl-5">
-            <p className="text-[24pt] font-extrabold justify-start ml-[5%] mr-[10%] mb-5 mt-17">Get in Touch</p>
-            <div className='flex'>
-              <div className='ml-[5%] mb-5 mt-9'>
-                <p className='text-[12pt]'>14, rue Charles </p> 
+      <div className="relative bg-[#505A28] w-[100%] md:w-[40%] text-[#F6F4DE] text-2xl flex flex-col pl-5">
+        <p className="text-[24pt] font-extrabold justify-start ml-[5%] mr-[10%] mb-5 mt-17">
+          Get in Touch
+        </p>
+        <div className="flex">
+          <div className="ml-[5%] mb-5 mt-9">
+            <p className="text-[12pt]">14, rue Charles </p>
 
-                <p className='text-[12pt]'>Bernhoeft, Kirchberg </p>
+            <p className="text-[12pt]">Bernhoeft, Kirchberg </p>
 
-                <p className='text-[12pt]'>Luxembourg </p>
+            <p className="text-[12pt]">Luxembourg </p>
 
-                <p className='text-[12pt]'>L-1240 </p>
-                <br></br>
-                <p className='text-[12pt]'>+352 621 495 483</p>
-                <p className='text-[12pt]'>nfo@sembezaafrica.org</p>
-              </div>
-            </div>
-            <div className='ml-[5%] mb-5 mt-0 flex gap-x-1.5'>
-              <a
-                href="https://www.linkedin.com/company/106016949/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="inline-block hover:opacity-80 transition-opacity"
-                aria-label={`Visit our linkedin (opens in new tab)`}
-              >
-                <Image 
-                  src="/logos/11062b_60c5fc4a3ecd49f2a697206b09eeace1~mv2.avif"
-                  height={30}
-                  width={30}
-                  alt='linkedin logo'
-                />
-              </a>
-              <a
-                href="https://www.instagram.com/sembezaafrica"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="inline-block hover:opacity-80 transition-opacity"
-                aria-label={`Visit our instagram (opens in new tab)`}
-              >
-                <Image 
-                  src="/logos/11062b_6e9638ad803e4099a6116eb750b5a584~mv2.avif"
-                  height={30}
-                  width={30}
-                  alt='instagram logo'
-                />
-              </a>
-              <a
-                href="https://www.youtube.com/@sembezaafarica"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="inline-block hover:opacity-80 transition-opacity"
-                aria-label={`Visit our youtube (opens in new tab)`}
-              >
-                <Image 
-                  src="/logos/11062b_30de4e7217e64dfdadc3ea03beea7b94~mv2.avif"
-                  height={30}
-                  width={30}
-                  alt='youtube logo'
-                />
-              </a>
-              <a
-                href="https://x.com/SembezaAfrica"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="inline-block hover:opacity-80 transition-opacity"
-                aria-label={`Visit our twitter (opens in new tab)`}
-              >
-                <Image 
-                  src="/logos/11062b_81cefb1bd2e2490d892a1cad5cc1cd8a~mv2.avif"
-                  height={30}
-                  width={30}
-                  alt='twitter logo'
-                />
-              </a>
-            </div>
+            <p className="text-[12pt]">L-1240 </p>
+            <br></br>
+            <p className="text-[12pt]">+352 621 495 483</p>
+            <p className="text-[12pt]">nfo@sembezaafrica.org</p>
+          </div>
         </div>
+        <div className="ml-[5%] mb-5 mt-0 flex gap-x-1.5">
+          <a
+            href="https://www.linkedin.com/company/106016949/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block hover:opacity-80 transition-opacity"
+            aria-label={`Visit our linkedin (opens in new tab)`}
+          >
+            <Image
+              src="/logos/11062b_60c5fc4a3ecd49f2a697206b09eeace1~mv2.avif"
+              height={30}
+              width={30}
+              alt="linkedin logo"
+            />
+          </a>
+          <a
+            href="https://www.instagram.com/sembezaafrica"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block hover:opacity-80 transition-opacity"
+            aria-label={`Visit our instagram (opens in new tab)`}
+          >
+            <Image
+              src="/logos/11062b_6e9638ad803e4099a6116eb750b5a584~mv2.avif"
+              height={30}
+              width={30}
+              alt="instagram logo"
+            />
+          </a>
+          <a
+            href="https://www.youtube.com/@sembezaafarica"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block hover:opacity-80 transition-opacity"
+            aria-label={`Visit our youtube (opens in new tab)`}
+          >
+            <Image
+              src="/logos/11062b_30de4e7217e64dfdadc3ea03beea7b94~mv2.avif"
+              height={30}
+              width={30}
+              alt="youtube logo"
+            />
+          </a>
+          <a
+            href="https://x.com/SembezaAfrica"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block hover:opacity-80 transition-opacity"
+            aria-label={`Visit our twitter (opens in new tab)`}
+          >
+            <Image
+              src="/logos/11062b_81cefb1bd2e2490d892a1cad5cc1cd8a~mv2.avif"
+              height={30}
+              width={30}
+              alt="twitter logo"
+            />
+          </a>
+        </div>
+      </div>
     </footer>
   );
 }
--- src/app/what-we-do/ubuntu/page.tsx
+++ src/app/what-we-do/ubuntu/page.tsx
@@ -9,7 +9,7 @@ export default function Ubuntu() {
       <Head>
         <title>Ubuntu</title>
       </Head>
-      <div className="bg-[#F6F4DE] min-h-screen">
+      <div className="bg-[#F6F4DE] min-h-screen pb-10">
         <div className="relative">
           {/* Background Image */}
           <Image
@@ -22,20 +22,20 @@ export default function Ubuntu() {
         </div>
         <div className="text-[#505A28] ">
           <div className="font-extrabold justify-center text-[30pt] mt-5 text-center">
-            <h1>Ubuntu Sports Festival:</h1>
+            <h1>Ubuntu Sports Festival</h1>
           </div>
-          <div className="font-bold text-[20pt] text-center">
+          <div className="font-bold text-[20pt] text-center mt-5">
             <h2> Uniting communities through sports and conservation</h2>
           </div>
         </div>
-        <div className="grid grid-cols-2 gap-8 text-[#505A28] mt-5 ">
-          <div>
-            <div className="font-extrabold text-[20pt] pl-5">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-10">
+          <div className="bg-[#505A28] rounded-lg ">
+            <div className="font-extrabold text-[20pt] pl-5 pt-10 text-center">
               <h2>Not just sports events</h2>
             </div>
-            <div className="font-bold text-[15pt] pl-5 pr-5">
+            <div className="font-bold text-[15pt] pl-5 pr-5 pt-5 pb-5">
               <p>
-                The Ubunto Sports Festival is much more than just a sports
+                The Ubuntu Sports Festival is much more than just a sports
                 event: it is a transformative experience based on the values of
                 cultural preservation, environmental sustainability, and
                 connecting diverse cultures and communities. Over the years, the
@@ -49,11 +49,11 @@ export default function Ubuntu() {
               </p>
             </div>
           </div>
-          <div>
-            <div className="font-extrabold text-[20pt] ">
+          <div className="bg-[#F69441] rounded-lg text-[#505A28]">
+            <div className="font-extrabold text-[20pt] text-center pl-5 pr-5 pt-10">
               <h2>Why Ubuntu?</h2>
             </div>
-            <div className="font-bold text-[15pt]">
+            <div className="font-bold text-[15pt] pl-5 pr-5 pt-5 pb-5">
               <p>
                 “Ubuntu” encapsulates the essence of community and
                 interconnectedness. This festival is built on the belief that
@@ -71,97 +71,128 @@ export default function Ubuntu() {
             </div>
           </div>
         </div>
-        <div className="font-extrabold text-center text-[30pt] text-[#505A28] mt-5">
-          <h1>.Our Mission.</h1>
+        <div className="font-extrabold text-center text-[30pt] text-[#505A28] mt-10 mb-0">
+          <h1>Our Mission</h1>
         </div>
-        <div className="text-[#505A28] font-semibold text-[15pt] mt-5 grid grid-cols-4 gap-8 pl-5 pr-5">
-          <div>
-            <p>
-              ​Fostering, persevering and practicing the concepts of unity and
-              collaboration: Respecting the central philosophy of Ubuntu, the
-              Festival promotes the values of interdependence and sharing, by
-              bringing together various communities and different people through
-              sports and games, as these do not look at social differences and
-              racial prejudices.
-            </p>
-          </div>
-          <div>
-            <p>
-              Preserving cultural heritage and traditions: Through traditional
-              games, such as Omweso, Morabaraba and Oware, we work to preserve
-              and especially promote Uganda&#39;s rich cultural history, so that the
-              latter is not pushed aside, but is recognized with the importance
-              it carries.
-            </p>
-          </div>
-          <div>
-            <p>
-              Promoting environmental conservation and the achievement of the
-              Sustainable Development Goals (SDGs) set by the United Nations:
-              Participants are educated and, if necessary, introduced to
-              sustainable conservation and protection practices for the
-              environment, communities and well-being.
-            </p>
-          </div>
-          <div>
-            <p>
-              Specifically, our goal is the achievement of the following SDGs:
-              i. SDG 3: Good Health and Well-Being. ii. SDG 4: Quality Education
-              iii. SDG 5: Gender Equality iv. SDG 10: Reduced Inequalities. v:
-              SDG 13: Climate Action vi: SDG 15: Life on Land By staking our
-              energies and efforts, we hope to achieve SDG 17: Partnership for
-              the Goals.
-            </p>
-          </div>
-        </div>
-        <div className="font-extrabold text-center text-[30pt] text-[#505A28] mt-5">
-          <h1>.Key Features.</h1>
-        </div>
-        <div className="text-[#505A28] font-bold text-[20pt] mt-5 grid grid-cols-3 gap-8 pl-5 pr-5">
-          <h3>1. Traditional games and sports</h3>
-          <h3>2. Conservation workshops</h3>
-          <h3>3. Cultural exchange</h3>
-        </div>
-        <div className="text-[#505A28] font-bold text-[20pt] mt-5 grid grid-cols-3 gap-8 pl-5 pr-5">
-          <h3>4. Global impact and volunteerism</h3>
-          <h3>5. Our partners and collaborators</h3>
-          <h3>6. Impact and next steps</h3>
-        </div>
-        <div className="bg-[#505A28] rounded-lg mt-10">
-          <div className="text-center font-extrabold text-[#F6F4DE] text-[30pt] mt-10">
-            <h1>.Achievements.</h1>
-          </div>
-          <div className="grid grid-cols-3 gap-8 mt-10">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mt-0 ">
+          <div className="text-[#505A28] font-bold text-[15pt] mt-0 pl-5 pr-5 pt-5 pb-5">
             <div>
-              <div className=" pl-10 text-[50pt] font-extrabold">
+              <p>
+                ​Fostering, persevering and practicing the concepts of unity and
+                collaboration: Respecting the central philosophy of Ubuntu, the
+                Festival promotes the values of interdependence and sharing, by
+                bringing together various communities and different people
+                through sports and games, as these do not look at social
+                differences and racial prejudices.
+              </p>
+            </div>
+          </div>
+          <div className="text-[#505A28] font-bold text-[15pt] mt-0 pl-5 pr-5 pt-5 pb-5">
+            <div>
+              <p>
+                Preserving cultural heritage and traditions: Through traditional
+                games, such as Omweso, Morabaraba and Oware, we work to preserve
+                and especially promote Uganda&#39;s rich cultural history, so
+                that the latter is not pushed aside, but is recognized with the
+                importance it carries.
+              </p>
+            </div>
+          </div>
+          <div className="text-[#505A28] font-bold text-[15pt] mt-0 pl-5 pr-5 pt-5 pb-5">
+            <div>
+              <p>
+                Promoting environmental conservation and the achievement of the
+                Sustainable Development Goals (SDGs) set by the United Nations:
+                Participants are educated and, if necessary, introduced to
+                sustainable conservation and protection practices for the
+                environment, communities and well-being.
+              </p>
+            </div>
+          </div>
+          <div className="text-[#505A28] font-bold text-[15pt] mt-0 pl-5 pr-5 pt-5 pb-5">
+            <div>
+              <p>
+                Specifically, our goal is the achievement of the following SDGs:
+                <p>
+                  <strong> i. </strong> SDG 3: Good Health and Well-Being.
+                </p>
+                <p>
+                  <strong> ii. </strong> SDG 4: Quality Education
+                </p>
+                <p>
+                  <strong> iii. </strong> SDG 5: Gender Equality
+                </p>
+                <p>
+                  <strong> iv. </strong> SDG 10: Reduced Inequalities.
+                </p>
+                <p>
+                  <strong> v: </strong> SDG 13: Climate Action
+                </p>
+                <strong> vi: </strong> SDG 15: Life on Land By staking our
+                energies and efforts, we hope to achieve SDG 17: Partnership for
+                the Goals.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="font-extrabold text-center text-[30pt] text-[#505A28] ">
+          <h1>Key Features</h1>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-3 pl-10 ">
+          <div className="text-[#505A28] font-bold text-[20pt] mt-5 ">
+            <h3>1. Traditional games and sports</h3>{" "}
+          </div>
+          <div className="text-[#505A28] font-bold text-[20pt] mt-5 ">
+            <h3>2. Conservation workshops</h3>{" "}
+          </div>
+          <div className="text-[#505A28] font-bold text-[20pt] mt-5 ">
+            <h3>3. Cultural exchange</h3>
+          </div>
+          <div className="text-[#505A28] font-bold text-[20pt] mt-5 ">
+            <h3>4. Global impact and volunteerism</h3>
+          </div>
+          <div className="text-[#505A28] font-bold text-[20pt] mt-5 ">
+            <h3>5. Our partners and collaborators</h3>
+          </div>
+          <div className="text-[#505A28] font-bold text-[20pt] mt-5 ">
+            <h3>6. Impact and next steps</h3>
+          </div>
+        </div>
+        <div className="bg-[#505A28] rounded-lg mt-10 p-5">
+          <div className="text-center font-extrabold text-[#F6F4DE] text-[30pt] mt-10">
+            <h1>Achievements</h1>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-5">
+            <div className="rounded-lg p-5">
+              <div className="text-[50pt] font-extrabold text-center mt-5">
                 <h1>1,000+</h1>
               </div>
-              <div className="pl-10 text-[15pt] font-semibold">
+              <div className="text-[15pt] font-semibold text-center">
                 <p>
                   Participants from Uganda and various international communities
                   engaged in sports, conservation, and cultural activities.
                 </p>
               </div>
             </div>
-            <div>
-              <div className=" pl-10 text-[50pt] font-extrabold">
+            <div className="rounded-lg p-5">
+              <div className="text-[50pt] font-extrabold text-center mt-5">
                 <h1>5</h1>
               </div>
-              <div className="pl-10 text-[15pt] font-semibold">
+              <div className="text-[15pt] font-semibold text-center">
                 <p>
                   International partnerships collaborating with organizations
                   from five different countries.
                 </p>
               </div>
             </div>
-            <div>
-              <div className=" pl-10 text-[50pt] font-extrabold">
+            <div className="rounded-lg p-5">
+              <div className="text-[50pt] font-extrabold text-center mt-5">
                 <h1>6</h1>
               </div>
-              <div className="pl-10 text-[15pt] font-semibold">
+              <div className="text-[15pt] font-semibold text-center mb-10">
                 <p>
                   Community-based partners working closely with six local
-                  community partners to ensure the festival&#39;s reach and
+                  community partners to ensure the festival's reach and
                   effectiveness in promoting unity and development.
                 </p>
               </div>
